27f72967478070c9d5fe1d7bde6ec012476e48fe
MichellevdWalt <michellevdwalt13@gmail.com>
2020-07-23 13:15:00 +0200

    Updates

---
+++ precache-manifest.cb83672dde9794af6db9992e3644ca16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378a99360c088f0967048324bb01dd47",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "924e5604242ee59ef3f3",
+    "url": "/react_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/react_app/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_app/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924e5604242ee59ef3f3",
+    "url": "/react_app/static/js/main.df3e1d95.chunk.js"
+  },
+  {
+    "revision": "215d5c1226ee235e8c3a",
+    "url": "/react_app/static/js/runtime-main.87741866.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
